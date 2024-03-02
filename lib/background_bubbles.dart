4cc57b5f3a5670d03cd59e96fd0cde7045af0c5d
dinesh kumar <dineshdeveloper29@gmail.com>
2024-03-02 20:46:37 +0530

    update documents

---
+++ lib/background_bubbles.dart
@@ -0,0 +1,7 @@
+library background_bubbles;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/src/example/android/app/src/main/kotlin/com/flutter/testproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.background_bubbles_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
